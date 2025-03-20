a2e581fda2c719cfd3a756cf3eabc1e4b1d2dc8a
Rafael-Del-Vaz <rafinha68taqua@gmail.com>
2025-03-20 19:43:41 -0300

    AMST5

--- Program.cs
+++ Program.cs
@@ -14,11 +14,6 @@ class Locacao : Carro
     public double Imposto { get; private set; }
     public double Total { get; private set; }
 
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> a44a669 (AMST5)
     public Locacao(string modelo, int horaInicial, int horaFinal)
     {
         Modelo = modelo;
@@ -40,39 +35,22 @@ class Locacao : Carro
     {
         Duracao = HoraFinal - HoraInicial;
 
-<<<<<<< HEAD
-       
-=======
 
->>>>>>> a44a669 (AMST5)
         if (Duracao <= 0)
         {
             Duracao += 24; 
         }
 
-<<<<<<< HEAD
-     
-=======
-
->>>>>>> a44a669 (AMST5)
         ValorLocacao = Duracao * ValorPorHora;
 
     
         Imposto = ValorLocacao <= 100 ? ValorLocacao * 0.20 : ValorLocacao * 0.15;
 
-<<<<<<< HEAD
 
         Total = ValorLocacao + Imposto;
     }
 
 
-=======
-      
-        Total = ValorLocacao + Imposto;
-    }
-
-   
->>>>>>> a44a669 (AMST5)
     public void ExibirRecibo()
     {
         Console.WriteLine("-Recibo da Locação-");
@@ -99,23 +77,14 @@ class Program
 
         Locacao locacao = new Locacao(modelo, horaInicial, horaFinal);
 
-<<<<<<< HEAD
-
-        locacao.ValidarHoras();
-
- 
-        locacao.CalcularValores();
 
 
-=======
-    
         locacao.ValidarHoras();
 
       
         locacao.CalcularValores();
 
     
->>>>>>> a44a669 (AMST5)
         locacao.ExibirRecibo();
     }
 }
